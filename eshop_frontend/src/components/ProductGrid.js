423ac856989b8e215ff3e4123f723e2bd7bfcfa3
Trang.BH210851@sis.hust.edu.vn <Trang.BH210851@sis.hust.edu.vn>
2024-06-09 18:11:14 +0700

    6h00

--- eshop_frontend/src/components/ProductGrid.js
+++ eshop_frontend/src/components/ProductGrid.js
@@ -2,35 +2,36 @@ import React, { useState } from 'react';
 import '../css/homepage.css';
 
 // real data
-const [products, setProducts] = useState([]);
-// const products = [
-//   // Sample data
-//   {
-//     id: 1,
-//     name: 'Product 1',
-//     price: 10.00,
-//     description: 'Description for product 1',
-//     stock: 20,
-//     images: [{ url: '1.jpg' }]
-//   },
-//   {
-//     id: 2,
-//     name: 'Product 2',
-//     price: 20.00,
-//     description: 'Description for product 2',
-//     stock: 30,
-//     images: [{ url: '2.jpg' }]
-//   },
-//   {
-//     id: 3,
-//     name: 'Product 3',
-//     price: 30.00,
-//     description: 'Description for product 3',
-//     stock: 40,
-//     images: [{ url: '3.jpg' }]
-//   },
-//   // Add more products as needed
-// ];
+// const [products, setProducts] = useState([]);
+
+// Sample data
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 10.00,
+    description: 'Description for product 1',
+    stock: 20,
+    images: [{ url: '1.jpg' }]
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 20.00,
+    description: 'Description for product 2',
+    stock: 30,
+    images: [{ url: '2.jpg' }]
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 30.00,
+    description: 'Description for product 3',
+    stock: 40,
+    images: [{ url: '3.jpg' }]
+  },
+  // Add more products as needed
+];
 
 const ProductGrid = () => {
   return (
