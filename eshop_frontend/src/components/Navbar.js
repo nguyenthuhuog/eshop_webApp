2848bf54488af3435510f2400891f0c06a0efbc6
Trang.BH210851@sis.hust.edu.vn <Trang.BH210851@sis.hust.edu.vn>
2024-06-09 15:51:10 +0700

    15h51

--- eshop_frontend/src/components/Navbar.js
+++ eshop_frontend/src/components/Navbar.js
@@ -37,6 +37,83 @@ const Navbar = () => {
           <marquee behavior="" direction="left">Sale news: Up to 50% off on selected items! ---- </marquee>
         </div>
       </div>
+      
+      <div className="main_container">
+        <div className="sidebar">
+          <div className="sidebar__inner">
+            <ul>
+              <li>
+                <a href="computer.html">
+                  <span className="icon"><i className="fas fa-laptop"></i></span>
+                  <span className="title">Computer & Laptop</span>
+                </a>
+              </li>
+              <li>
+                <a href="keyboard.html">
+                  <span className="icon"><i className="fas fa-keyboard"></i></span>
+                  <span className="title">Keyboards</span>
+                </a>
+              </li>
+              <li>
+                <a href="mouse.html">
+                  <span className="icon"><i className="fas fa-computer-mouse"></i></span>
+                  <span className="title">Mouses</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="container">
+          <div className="main">
+            <div className="banner">
+              <img src="img/banner/1.png" className="visible" alt="Banner 1"/>
+              <img src="img/banner/2.jpg" className="hidden" alt="Banner 2"/>
+              <img src="img/banner/3.jpg" className="hidden" alt="Banner 3"/>
+              <img src="img/banner/4.jpg" className="hidden" alt="Banner 4"/>
+              <img src="img/banner/5.png" className="hidden" alt="Banner 5"/>
+            </div>
+            
+            <div className="item">
+              <section className="sale-news">
+                <h2>Sale News</h2>
+                <ul>
+                  <li><i className="fas fa-fire"></i> Up to 50% off on selected items!</li>
+                  <li><i className="fas fa-truck"></i> Free shipping on orders over $100!</li>
+                  <li><i className="fas fa-star"></i> New arrivals are in stock now!</li>
+                </ul>
+              </section>
+            </div>
+            
+            <div className="product-grid">
+              <div className="product">
+                <img src="1.jpg" alt="Product 1"/>
+                <p>Product 1</p>
+                <p>$10.00</p>
+              </div>
+              <div className="product">
+                <img src="1.jpg" alt="Product 2"/>
+                <p>Product 2</p>
+                <p>$20.00</p>
+              </div>
+              <div className="product">
+                <img src="1.jpg" alt="Product 3"/>
+                <p>Product 3</p>
+                <p>$30.00</p>
+              </div>
+            </div>
+            
+            <div className="item" id="news">
+              <h2>Don't Forget!!!</h2>
+              <ul>
+                <li><i className="fas fa-envelope"></i> Sign up for our newsletter and get 10% off your first order!</li>
+                <li><i className="fas fa-blog"></i> Check out our new blog post on the latest tech trends!</li>
+                <li><i className="fas fa-share-alt"></i> Follow us on social media for exclusive deals and updates!</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
